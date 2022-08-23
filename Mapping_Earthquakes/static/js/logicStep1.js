6291efa01e7611ffc245c9999a67d0e4cb76fbc6
John Hendricks <jwh8zc@virginia.edu>
2022-08-23 12:33:01 -0400

    made basic map of earthquakes

---
+++ Mapping_Earthquakes/static/js/logicStep1.js
@@ -0,0 +1,55 @@
+// Add console.log to check to see if our code is working.
+console.log("working");
+
+
+let streets = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/streets-v11/tiles/{z}/{x}/{y}?access_token={accessToken}', {
+attribution: 'Map data © <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery (c) <a href="https://www.mapbox.com/">Mapbox</a>',
+    maxZoom: 18,
+    accessToken: API_KEY
+});
+
+
+// We create the dark view tile layer that will be an option for our map.
+let satelliteStreets = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/satellite-streets-v11/tiles/{z}/{x}/{y}?access_token={accessToken}', {
+attribution: 'Map data © <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery (c) <a href="https://www.mapbox.com/">Mapbox</a>',
+    maxZoom: 18,
+    accessToken: API_KEY
+});
+
+let baseMaps = {
+    "Streets": streets,
+    "Satellite": satelliteStreets
+};
+
+let map = L.map('mapid', {
+    center: [39.5, -98.5],
+    zoom: 3,
+    layers: [streets]
+});
+
+L.control.layers(baseMaps).addTo(map);
+
+
+
+
+
+
+
+// Then we add our 'graymap' tile layer to the map
+streets.addTo(map);
+
+// Accessing the Toronto airline routes GeoJSON URL.
+let url = "https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_week.geojson";
+
+// Grabbing our GeoJSON data
+d3.json(url).then(function(data) {
+    console.log(data);
+
+// let myStyle = {
+//     color: "#ffffa1",
+//     weight: 2
+// }
+    
+    //Creating a GeoJson layer with the retrieved data
+    L.geoJSON(data).addTo(map);
+});
